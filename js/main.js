34b09c756d8999bf92a4708644f013654185d1d6
SERGIO HEREDIA <sergio95_h@hotmail.com>
2025-07-11 02:21:10 -0300

    ejemplo de true y false

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 alert ()
 
 
+
+let calificacion = 75;
+
+if (calificacion >= 90) {
+    console.log("Excelente");
+} else if (calificacion >= 70) {
+    console.log("Bueno");
+} else {
+    console.log("Necesitas estudiar más");
+}
+
+// EJEMPLO DE MAYOR Y MENOR 
+
+let edad = 20;
+let acceso = (edad >= 18) && (edad <= 30);
+console.log(acceso); // true si la edad está entre 18 y 30
+let acceso = (edad)
+
+
